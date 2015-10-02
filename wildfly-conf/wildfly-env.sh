09633b91e7084e0524cf99d04b42214fbc88e563
peder280370 <peder@carolus.dk>
2015-10-02 12:12:29 +0200

    Remove debugging

--- wildfly-conf/wildfly-env.sh
+++ wildfly-conf/wildfly-env.sh
@@ -5,8 +5,6 @@ pushd `dirname ${BASH_SOURCE}` > /dev/null
 export WILDFLY_CONF_DIR=`pwd`
 popd > /dev/null
 
-echo "WCD $WILDFLY_CONF_DIR"
-
 export WILDFLY_VERSION=9.0.1.Final
 export WILDFLY=wildfly-$WILDFLY_VERSION
 export WILDFLY_ROOT=$WILDFLY_CONF_DIR/..
